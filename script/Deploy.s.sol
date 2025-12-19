f7a3fcfa7170f9e4af1169fb7aa61a072086cc4b
rayenharhouri <rayenharhouri99@gmail.com>
2025-12-19 17:12:02 +0100

    update deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,22 +3,26 @@ pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
 import "../contracts/MemberRegistry.sol";
-import "../contracts/GoldAssetToken.sol";
 import "../contracts/GoldAccountLedger.sol";
+import "../contracts/GoldAssetToken.sol";
 
 contract DeployGIFT is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
         vm.startBroadcast(deployerPrivateKey);
 
         MemberRegistry memberRegistry = new MemberRegistry();
-        console.log("DEPLOYED_MEMBER_REGISTRY=%s", address(memberRegistry));
+        console.log("MemberRegistry:", address(memberRegistry));
 
         GoldAccountLedger accountLedger = new GoldAccountLedger(address(memberRegistry));
-        console.log("DEPLOYED_GOLD_ACCOUNT_LEDGER=%s", address(accountLedger));
+        console.log("GoldAccountLedger:", address(accountLedger));
 
-        GoldAssetToken goldAssetToken = new GoldAssetToken(address(memberRegistry), address(accountLedger));
-        console.log("DEPLOYED_GOLD_ASSET_TOKEN=%s", address(goldAssetToken));
+        GoldAssetToken goldAssetToken = new GoldAssetToken(
+            address(memberRegistry),
+            address(accountLedger)
+        );
+        console.log("GoldAssetToken:", address(goldAssetToken));
 
         vm.stopBroadcast();
     }
